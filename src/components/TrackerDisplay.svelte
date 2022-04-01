6c89fbeb1f0e79513bf52d8d57711628c5e0fd93
notevenagoat <itinoco@pacificstream.com.au>
2022-04-01 16:46:53 +1100

    made the button disabled based on reactiveness of selected

--- src/components/TrackerDisplay.svelte
+++ src/components/TrackerDisplay.svelte
@@ -4,13 +4,26 @@
   const addMook = () => {
     trackedMooks.update((contents) => [...contents, $mooks[$selectedMook]]);
   };
+
+let selectionValidate
+  selectedMook.subscribe(value => {
+		selectionValidate = value;
+	});
+
+  function validation(selected) {
+    if (selected === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 </script>
 
 <main>
-  <button on:click={addMook}> Add Mook to Tracker </button>
+  <button disabled={validation($selectedMook)} on:click={addMook}>
+    Add Mook to Tracker
+  </button>
 
-
-  
   {#each $trackedMooks as tracked}
     <div>{tracked.name} added to list</div>
   {/each}
